788d999ba0098eb565fc750c989dbdf056136f27
= <=>
2015-11-24 22:55:02 +0000

    hack in some better propogation constants

--- sim/skylightSim/src/Puddle.cpp
+++ sim/skylightSim/src/Puddle.cpp
@@ -41,7 +41,7 @@ void Puddle::propagate(float input[leds_width][leds_height][colour_width],
 	float averagePosition[colour_width] = {0};
 	float propagationConstants[colour_width] = {0.11,0.1,0.09}; // RGB
 	float dampingConstants[colour_width] = {0.01,0.01,0.01}; // RGB
-	static const float kernel[kernel_size][kernel_size] = {{1,1,1,1,1},{1,1,1,1,1},{1,1,0,1,1},{1,1,1,1,1},{1,1,1,1,1}};
+	static const float kernel[kernel_size][kernel_size] = {{1,1,1,1,1},{1,2,2,2,1},{1,2,0,2,1},{1,2,2,2,1},{1,1,1,1,1}};
     static const int kOffset = kernel_size / 2;
 
 	assert(kernel_size % 2 == 1);
@@ -64,7 +64,7 @@ void Puddle::propagate(float input[leds_width][leds_height][colour_width],
 
 	for (int i = 0; i < colour_width; i++)
 	{
-		averagePosition[i] /= kernel_size * kernel_size - 1;
+		averagePosition[i] /= kernel_size * kernel_size + 7;
 		float acceleration = (averagePosition[i] - input[x][y][i]) * propagationConstants[i];
 		velocity[x][y][i] += acceleration - (velocity[x][y][i] * dampingConstants[i]);
 		output[x][y][i] = input[x][y][i] + velocity[x][y][i];
